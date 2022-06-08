1a81aeb041898aaf4a1dc93c203cc10bdd668491
ninadsutrave <ninadsutrave@gmail.com>
2022-06-08 12:29:58 +0530

    colors added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Body from './Components/Body.js'
+import Body from './components/Body.js'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <>
       {/* <Header /> */}
