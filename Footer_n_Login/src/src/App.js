57e58bf0a9c0651b587bc430a34f1ea4f5b8d958
Him-dhiman <62401406+Him-dhiman@users.noreply.github.com>
2022-06-08 10:41:22 +0530

    Add files via upload

---
+++ Footer_n_Login/src/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './Components/footer';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <div>
+       <button type="submit" class="btn btn-primary mb-4">
+              Login
+            </button>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
