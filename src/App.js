39f3f54aec4507ff58a0ee66da7f286953237aac
sudhanshu-hbk <2019csb1123@iitrpr.ac.in>
2022-06-09 12:35:11 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+import SignInAndSignUp from "./pages/login-register/design";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <LandingPage />
+              </>
+            }
+          />
+          <Route
+            path="/sign-in"
+            element={
+              <>
+              <SignInAndSignUp />
+              </>
+            }
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
