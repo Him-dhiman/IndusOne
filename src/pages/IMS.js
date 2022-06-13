c19686c41c9de8d1444cd5c772577514c5f63742
Him-dhiman <2019meb1262@iitrpr.ac.in>
2022-06-14 01:36:58 +0530

    Changed IMS.js

--- src/pages/IMS.js
+++ src/pages/IMS.js
@@ -1,12 +1,46 @@
 import React from 'react';
-
-
-function IMS() {
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBAccordion, MDBAccordionItem } from 'mdb-react-ui-kit';
+export default function App() {
   return (
-      <>
-    
-      </>
+    <div>
+    <MDBAccordion flush initialActive={1}>
+      <MDBAccordionItem collapseId={1} headerTitle='Accordion Item #1'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </MDBAccordionItem>
+      <MDBAccordionItem collapseId={2} headerTitle='Accordion Item #2'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </MDBAccordionItem>
+      <MDBAccordionItem collapseId={3} headerTitle='Accordion Item #3'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </MDBAccordionItem>
+    </MDBAccordion>
+    <MDBCard style={{ maxWidth: '22rem' }}>
+      <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+      <MDBCardBody>
+        <MDBCardTitle>Card title</MDBCardTitle>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </MDBCardText>
+        <MDBBtn href='#'>Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+    </div>
+
   );
 }
-
-export default IMS;
