9eac8bedc7bad799a6c452ef5ae8ca0eb3844088
Him-dhiman <2019meb1262@iitrpr.ac.in>
2022-06-14 13:54:37 +0530

    Changes made to IMS page

--- src/pages/IMS.js
+++ src/pages/IMS.js
@@ -1,46 +1,34 @@
 import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
-import { MDBAccordion, MDBAccordionItem } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
+import Carousel from '../components/Carousel';
+import CardItem from '../components/MDB_Cards';
+
 export default function App() {
   return (
-    <div>
-    <MDBAccordion flush initialActive={1}>
-      <MDBAccordionItem collapseId={1} headerTitle='Accordion Item #1'>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </MDBAccordionItem>
-      <MDBAccordionItem collapseId={2} headerTitle='Accordion Item #2'>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </MDBAccordionItem>
-      <MDBAccordionItem collapseId={3} headerTitle='Accordion Item #3'>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </MDBAccordionItem>
-    </MDBAccordion>
-    <MDBCard style={{ maxWidth: '22rem' }}>
-      <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
-      <MDBCardBody>
-        <MDBCardTitle>Card title</MDBCardTitle>
-        <MDBCardText>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </MDBCardText>
-        <MDBBtn href='#'>Button</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
+    <div style={{padding :50}}>
+    <Carousel />
+    <MDBRow style={{padding :50}}>
+      <MDBCol sm='3'><CardItem
+      src="images/img-1.jpg"
+      title="Learning content from more than 1000 companies in the form of challenges."
+      content="Learn"
+      /> </MDBCol>
+      <MDBCol sm='3'><CardItem
+      src="images/img-1.jpg"
+      title="Learning content from more than 1000 companies in the form of challenges."
+      content="Learn"
+      /> </MDBCol>
+      <MDBCol sm='3'><CardItem
+        src="images/img-1.jpg"
+        title="Learning content from more than 1000 companies in the form of challenges."
+        content="Learn"
+      /> </MDBCol>
+      <MDBCol sm='3'><CardItem
+      src="images/img-1.jpg"
+      title="Learning content from more than 1000 companies in the form of challenges."
+      content="Learn"
+      /> </MDBCol>
+    </MDBRow>
     </div>
-
   );
 }
