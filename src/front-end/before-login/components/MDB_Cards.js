6876ef1058d906378a278b2ee4d2b5307d0be157
Him-dhiman <2019meb1262@iitrpr.ac.in>
2022-06-27 17:06:30 +0530

    changes

--- src/front-end/before-login/components/MDB_Cards.js
+++ src/front-end/before-login/components/MDB_Cards.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
-//
-// export default function App(props) {
-//   return (
-//     <MDBCard style={{ maxWidth: '22rem' }}>
-//       <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-//         <MDBCardImage src={props.src} fluid alt='...' />
-//         <a>
-//           <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-//         </a>
-//       </MDBRipple>
-//       <MDBCardBody>
-//         <MDBCardTitle>{props.title}</MDBCardTitle>
-//         <MDBCardText>
-//         {props.content}
-//         </MDBCardText>
-//         <MDBBtn href='#'>Button</MDBBtn>
-//       </MDBCardBody>
-//     </MDBCard>
-//   );
-// }
+import React from 'react';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+
+export default function App(props) {
+  return (
+    <MDBCard style={{ maxWidth: '22rem' }}>
+      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+        <MDBCardImage src={props.src} fluid alt='...' />
+        <a>
+          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+        </a>
+      </MDBRipple>
+      <MDBCardBody>
+        <MDBCardTitle>{props.title}</MDBCardTitle>
+        <MDBCardText>
+        {props.content}
+        </MDBCardText>
+        <MDBBtn href='#'>Button</MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+  );
+}
