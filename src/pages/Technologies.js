aca15ce20a4f7ab2990340611c56d1fe2f2b21d1
Him-dhiman <2019meb1262@iitrpr.ac.in>
2022-06-27 09:36:53 +0530

    Changes

---
+++ src/pages/Technologies.js
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBBtn } from 'mdb-react-ui-kit';
+// import Carousel from '../components/Carousel';
+// import CardItem from '../components/MDB_Cards';
+//
+// function Technologies() {
+//   return (
+//       <>
+//       <div style={{padding :50}}>
+//       <Carousel />
+//       <MDBRow style={{padding :50}}>
+//         <MDBCol sm='3'><CardItem
+//         src="images/img-1.jpg"
+//         title="Learning content from more than 1000 companies in the form of challenges."
+//         content="Learn"
+//         /> </MDBCol>
+//         <MDBCol sm='3'><CardItem
+//         src="images/img-1.jpg"
+//         title="Learning content from more than 1000 companies in the form of challenges."
+//         content="Learn"
+//         /> </MDBCol>
+//         <MDBCol sm='3'><CardItem
+//           src="images/img-1.jpg"
+//           title="Learning content from more than 1000 companies in the form of challenges."
+//           content="Learn"
+//         /> </MDBCol>
+//         <MDBCol sm='3'><CardItem
+//         src="images/img-1.jpg"
+//         title="Learning content from more than 1000 companies in the form of challenges."
+//         content="Learn"
+//         /> </MDBCol>
+//       </MDBRow>
+//       </div>
+//       </>
+//   );
+// }
+//
+// export default Technologies;
